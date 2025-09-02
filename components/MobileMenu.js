70f6bc09361c8d99e4461456c86fec1e0c8e967b
Onchukov Konstantin <legis3@rambler.ru>
2025-09-02 21:33:41 +0500

    Neural Automation true name

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -44,7 +44,7 @@ const MobileMenu = ({ isOpen, onClose, activeTab, setActiveTab }) => {
 
               {/* Logo */}
               <div className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-8">
-                NeuralAutomate
+                Neural Automation
               </div>
 
               {/* Menu Items */}
